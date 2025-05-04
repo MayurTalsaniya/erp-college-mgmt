d62fef8a4bf50959f46ecbd49af7fd0431c94703
Mayur <mayurtalsaniya@hotmail.com>
2025-05-04 19:58:33 +0530

    staff module update

--- student_management_app/EmailBackEnd.py
+++ student_management_app/EmailBackEnd.py
@@ -1,15 +1,26 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.backends import ModelBackend
+#class EmailBackEnd(ModelBackend):
+#    def authenticate(self,username=None, password=None, **kwargs):
+#        UserModel=get_user_model()
+ #       try:
+#            user=UserModel.objects.get(email=username)
+ #       except UserModel.DoesNotExist:
+ #           return None
+ #       else:
+ #           if user.check_password(password):
+ #               return user
+ #       return None
 
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
 
 class EmailBackEnd(ModelBackend):
-    def authenticate(self,username=None, password=None, **kwargs):
-        UserModel=get_user_model()
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
         try:
-            user=UserModel.objects.get(email=username)
+            user = UserModel.objects.get(email=username)
         except UserModel.DoesNotExist:
             return None
         else:
-            if user.check_password(password):
+            if user.check_password(password) and self.user_can_authenticate(user):
                 return user
         return None
