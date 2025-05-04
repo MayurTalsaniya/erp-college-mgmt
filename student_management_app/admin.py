dd9c578f42096d7ba29f905b0ee706e4814ce2d8
Mayur <mayurtalsaniya@hotmail.com>
2025-05-04 17:43:34 +0530

    admin log in fixed

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -1,12 +1,32 @@
+# from django.contrib import admin
+
+# # Register your models here.
+# from django.contrib.auth.admin import UserAdmin
+
+# from student_management_app.models import CustomUser
+
+# class UserModel(UserAdmin):
+#     pass
+
+# admin.site.register(CustomUser,UserModel)
+
 from django.contrib import admin
-
-# Register your models here.
 from django.contrib.auth.admin import UserAdmin
-
 from student_management_app.models import CustomUser
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
 
-class UserModel(UserAdmin):
-    pass
+    # This makes sure required fields are displayed in the admin
+    list_display = ['username', 'email', 'first_name', 'last_name', 'user_type', 'is_staff', 'is_superuser']
+    list_filter = ['user_type', 'is_staff', 'is_superuser']
 
-admin.site.register(CustomUser,UserModel)
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('user_type',)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('user_type',)}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
