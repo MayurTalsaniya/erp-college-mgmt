38a7af2a039bedaf96cb5d3d2a22a49f5542e09d
Mayur <mayurtalsaniya@hotmail.com>
2025-05-04 21:26:35 +0530

    deployment ready

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -87,11 +87,16 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+}
+
+#DATABASES = {
+#    'default': {
         #=====Enable Only Making Project Live on Heroku====
     
-        dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
+#        dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
     
         # 'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
@@ -101,8 +106,8 @@ DATABASES = {
         #'PASSWORD':'student_management_password',
         #'HOST':'localhost',
         #'PORT':'3306'
-    }
-}
+#    }
+#}
 
 
 # Password validation
@@ -166,11 +171,6 @@ EMAIL_FILE_PATH=os.path.join(BASE_DIR,"sent_mails")
 
 #prod_db=dj_database_url.config(conn_max_age=500)
 #DATABASES['default'].update(prod_db)
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
-
-
-
 
 #added by me
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
